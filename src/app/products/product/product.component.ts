ed46981db8fe6299ad54f4b65c1b8d685498446a
JaraPava <alberto-11111@live.com.mx>
2024-04-18 10:46:38 -0600

    Se declaran las propiedades publicas de la clase ProductComponent, ádemas se agregarón los métodos para cambiar el titulo, el precio y resetear los valores a su estado original

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -6,10 +6,36 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  title:string = "";
-  price:number = 0;
-  description:string = "";
-  category:string = "";
-  image:string = "";
+  public title:string = "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops";
+  public price:number = 109.95;
+  public description:string = "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday";
+  public category:string = "men's clothing";
+  public image:string = "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg";
+
+  //Los getters son métodos que parece propiedad
+  public capitalizedName():string{
+    return this.title.toUpperCase();
+  };
+
+  getProductDescription():string{
+    //Concatemamos con la interpolación el titulo y la descripción
+    return `${this.title} - ${this.description}`
+  };
+
+  changeTitle():void{
+    // //TODO:
+    // throw 'Método no implementado' //Cuando se va a implementar un método pero aún no se implementado podemos poner este throw o un //TODO:
+
+    this.title = "Producto 1"
+  };
+
+  changePrice():void{
+    this.price = 10.23
+  };
+
+  reset():void{
+    this.title = "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops";
+    this.price = 109.95;
+  }
 
 }
