61e184ded89693f2010a15c03d2bf96a8332b818
JaraPava <alberto-11111@live.com.mx>
2024-04-18 13:46:57 -0600

    Se importan el módulo de producto en el app.module.ts (modulo principal)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './products/product/product.component';
-import { ListComponent } from './products/list/list.component';
 
 import { CounterModule } from './counter/counter.module';
+import { ProductModule } from './products/product.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
-    ListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    CounterModule
+    // AppRoutingModule,
+    CounterModule,
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
