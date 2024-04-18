3bf46c9c68a51f2a3f197e645463e1589edf8e22
JaraPava <alberto-11111@live.com.mx>
2024-04-17 18:34:42 -0600

    Se agrega comentario en los styles globales

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+/*
+  Este archivo es el punto de entrada en Angular
+  Hay veces que se estara trabajando con AngularUniversal, AngularElectronic.
+  Angular + otra tecnología nos permitira hacer aplicaciones de diferentes plataformas
+*/
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
