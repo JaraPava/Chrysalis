c7f241957c07c69290ec9be40d60b25c617eb046
JaraPava <alberto-11111@live.com.mx>
2024-04-18 11:52:11 -0600

    Se agregó un array llamado productNames de tipo string, en el cual contiene un array de los nombres de productos y este se usa en el .html de este mismo componente

--- src/app/products/list/list.component.ts
+++ src/app/products/list/list.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  public productNames: string[] = [
+    'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    'Mens Casual Premium Slim Fit T-Shirts',
+    'Mens Cotton Jacket',
+    'Mens Casual Slim Fit'
+  ]
 }
