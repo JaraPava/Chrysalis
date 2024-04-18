8b9c47e887f1f5b15c3af56322a902477609b53b
JaraPava <alberto-11111@live.com.mx>
2024-04-18 07:44:47 -0600

    Se generan componentes con los comandos que te proporciona Angular

---
+++ src/app/products/list/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+}
