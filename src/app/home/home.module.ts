7391bf8b559ba1e9b5254f7a32fc6b972665b92e
JaraPava <alberto-11111@live.com.mx>
2024-04-19 06:21:30 -0600

    Se importa el módulo de producto y se importa en el módulo de Home :0

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ProductModule } from '../products/product.module';
 
 
 
@@ -13,6 +14,7 @@ import { MainPageComponent } from './pages/main-page.component';
   ],
   imports: [
     CommonModule,
+    ProductModule
   ]
 })
 export class HomeModule { }
