b35ed4e97a49e724d2b9346c9df29fc04adc2c91
JaraPava <alberto-11111@live.com.mx>
2024-04-19 08:34:30 -0600

    En el main-page.component.ts se agrego un propiedad publica llamada products que tiene dos productos además esta haciendo uso del Interface Product

--- src/app/products/components/list/list.component.ts
+++ src/app/products/components/list/list.component.ts
@@ -1,23 +1,29 @@
 import { Component } from '@angular/core';
+import { Product } from '../../../home/interfaces/product.interface';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
-export class ListComponent{
-  public productNames: string[] = [
-    'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-    'Mens Casual Premium Slim Fit T-Shirts',
-    'Mens Cotton Jacket',
-    'Mens Casual Slim Fit'
-  ];
+export class ListComponent {
 
-  public deletedProductName?:string;
+  public productList: Product[] = [{
+    title: "Mens Casual Slim Fit",
+    price: 15.99,
+    description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    category: "men's clothing",
+    image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    rating: {
+      rate: 2.1,
+      count: 430
+    }
+  }]
+  // public deletedProductName?:string;
 
-  removeLastProduct():void{
-    const deletedProduct = this.productNames.pop();
-    this.deletedProductName = deletedProduct;
-    // this.productNames.pop();//Regresa un string o undefined por que si no borra nada el undefined es su valor
-  }
+  // removeLastProduct():void{
+  //   const deletedProduct = this.productNames.pop();
+  //   this.deletedProductName = deletedProduct;
+  //   // this.productNames.pop();//Regresa un string o undefined por que si no borra nada el undefined es su valor
+  // }
 }
