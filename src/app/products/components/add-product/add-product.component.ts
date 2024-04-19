7d663bbd649b0070b5a8fa355e0f06ab37e96a9f
JaraPava <alberto-11111@live.com.mx>
2024-04-19 10:12:17 -0600

    Se crea una propiedad product que tiene una producto por defecto en mi .ts del componente AddProductComponent y en el .html se hace el binding con el [(ngModel)]

--- src/app/products/components/add-product/add-product.component.ts
+++ src/app/products/components/add-product/add-product.component.ts
@@ -1,9 +1,23 @@
 
 import { Component } from '@angular/core';
+import { Product } from '../../../home/interfaces/product.interface';
 
 @Component({
   selector: 'products-add-product',
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css',
 })
-export class AddProductComponent { }
+export class AddProductComponent {
+
+  public product:Product = {
+    title:'',
+    price:0,
+    category:'',
+    description:'',
+    image:'',
+    rating:{
+      count:0,
+      rate:0
+    }
+  }
+}
