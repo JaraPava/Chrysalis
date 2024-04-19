12bbfbb56a94789d32203ae35ce5730c6ddde0b7
JaraPava <alberto-11111@live.com.mx>
2024-04-19 06:19:58 -0600

    Se agrega se declara y exporta el componente add-producto al modúlo de productos

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from "@angular/common";
 
 import { ListComponent } from "./components/list/list.component";
 import { ProductComponent } from "./components/product/product.component";
+import { AddProductComponent } from './components/add-product/add-product.component';
 
 @NgModule({
   declarations:[
     ListComponent,
     ProductComponent,
+    AddProductComponent
   ],
   exports:[
     ListComponent,
-    ProductComponent
+    ProductComponent,
+    AddProductComponent
   ],
   imports:[
     CommonModule
   ]
 })
 export class ProductModule{}
+
